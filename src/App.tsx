1e747b569e6cffa7ede7b2424d7baf90a56f74f4
pripri <kathelenpmachado@gmail.com>
2024-08-30 23:19:15 -0300

    subindo alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
-import { Button } from "./components/Button";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./components/Router";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-    <Button variant="primary" />
-    <Button variant="secondary" />
-    <Button variant="danger"/>
-    <Button variant="success"/>
-    <Button />
-
+      <BrowserRouter>
+        <Router />
+        </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
