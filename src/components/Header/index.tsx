c7d1a48edc72c8186b7ec654c593145bdefbbdd7
pripri <kathelenpmachado@gmail.com>
2024-09-25 12:22:45 -0300

    header e layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from "./styles";
+import logoIgnite from '../../assets/Logo-ignite.svg';
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header () {
     return (
         <HeaderContainer>
-            <span>logo</span>
+            <img src={logoIgnite} alt="" />
             <nav>
-                <a href=""> timer </a>
-                <a href=""> history </a>
+                <NavLink to="/" title="timer"> 
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink to="/history" title="histórico">
+                    <Scroll size={24} />
+                </NavLink>
             </nav>
         </HeaderContainer>
     )
